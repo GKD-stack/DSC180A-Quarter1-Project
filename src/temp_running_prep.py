ad9d41a38f1bf6d016b386d9e94e0faf517219ae
gurman-dhaliwal <62931173+GKD-stack@users.noreply.github.com>
2023-12-11 14:14:09 -0800

    Create temp_running_prep.py

---
+++ src/temp_running_prep.py
@@ -0,0 +1,3 @@
+prepare_summary_stats_for_prs("alz_data.txt", ['SNP', 'A1', 'BETA'], "alz_data.gwas.txt")
+prepare_summary_stats_for_prs("megastroke.txt", ['MarkerName', 'Allele1', 'Effect'], "megastroke.gwas.txt")
+prepare_summary_stats_for_prs(input_file_name, ['rs_number', 'reference_allele', 'beta'], "bone.gwas.txt")
